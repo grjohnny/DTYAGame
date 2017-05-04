119c117fa16c27d839a1e626424895e679344637
Zihuang Guo <zguo30@illinois.edu>
2017-05-04 12:53:34 -0500

    upload dtya game - uiuc cs196

---
+++ Assets/Trump Assets/Scripts/WallDust.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDust : MonoBehaviour {
+
+    float offsetX = 4f;
+    float offsetY = 8f;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (WallAttack.getIsMoving())
+        {
+            GameObject.Find("DustStorm").GetComponent<ParticleSystem>().Play();
+            Debug.Log("Dust is active!");
+            GameObject.Find("DustStorm").transform.position = new Vector2(WallAttack.getWallPosX() + offsetX, WallAttack.getWallPosY() + offsetY);
+        }
+	}
+}
